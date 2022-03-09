f4b28980c6bbff9e99b7661158c3200fcbf1b3a7
eungyeollee <leepro225@gmail.com>
2022-03-09 13:44:10 +0900

    add count function

--- src/calculator.js
+++ src/calculator.js
@@ -1,21 +1,24 @@
 class Calculator {
     constructor() {
-
+        this.value = 0;
     }
 
-    set() {
-
+    set(target) {
+        target.innerText = this.value;
     }
 
-    add() {
-
+    add(num) {
+        this.value += 1; 
     }
 
-    subtract() {
-
+    subtract(num) {
+        this.value -= 1;
     }
 
     reset() {
-
+        this.value = 0;
     }
 }
+
+
+
